8033283a2d5a8655e651af2fff02f84f06d345ef
tweber <tom-vibrant@gmx.de>
2016-11-09 22:41:09 +0100

    .

--- src/de/tunetown/nnpg/model/ModelProperties.java
+++ src/de/tunetown/nnpg/model/ModelProperties.java
@@ -3,7 +3,7 @@ package de.tunetown.nnpg.model;
 public class ModelProperties {
 	public static final double DATAPANEL_SAMPLES_RANGE = 10.0;
 	public static final double DATAPANEL_ERASE_RADIUS = 0.5;
-	public static final double DATAPANEL_DOUBLERATE = 1.2;
+	public static final double DATAPANEL_DOUBLERATE = 1.1;
 	public static final double DATAPANEL_DOUBLERADIUS = 0.5;
 	
 	public static final int[] NETWORK_DEFAULT_TOPOLOGY = {2, 4, 4, 4, 1};
