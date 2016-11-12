a3a4e156bdcbe7e05d6d6f5ad544dd817549ef66
tweber <tom-vibrant@gmx.de>
2016-11-12 15:14:16 +0100

    .

---
+++ src/de/tunetown/nnpg/model/neuroph/NeurophDataWrapper.java
@@ -0,0 +1,184 @@
+package de.tunetown.nnpg.model.neuroph;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.dkriesel.snipe.training.TrainingSampleLesson;
+import de.tunetown.nnpg.model.DataContainer;
+import de.tunetown.nnpg.model.DataWrapper;
+
+/**
+ * Wrapper for SNIPE training data
+ * 
+ * @author xwebert
+ *
+ */
+public class NeurophDataWrapper extends DataWrapper{
+
+	private TrainingSampleLesson trainingLesson;
+	private TrainingSampleLesson testLesson;
+
+	@Override
+	public void addSample(double x, double y, double value) {
+		double[][] in = getMergedInputs();
+		double[][] teach = getMergedDesiredOutputs();
+
+		double[][] nin;
+		double[][] nteach;
+
+		if (in == null) {
+			nin = new double[1][2];
+			nteach = new double[1][1];
+			
+			nin[0][0] = x;
+			nin[0][1] = y;
+			nteach[0][0] = value;
+		} else {
+			nin = new double[in.length + 1][2];
+			nteach = new double[in.length + 1][1];
+			
+			for(int n=0; n<in.length; n++) {
+				nin[n] = in[n];
+				nteach[n] = teach[n];
+				
+			}
+			
+			double[] ninl = {x, y};
+			nin[in.length] = ninl;
+			double[] ntl = {value};
+			nteach[in.length] = ntl;
+		}
+		
+		setLesson(new TrainingSampleLesson(nin, nteach));		
+	}
+	
+	@Override
+	public void deleteSamplesAroundPoint(double x, double y, double radius) {
+		if(!hasData()) return;
+		
+		double[][] in = getMergedInputs();
+		double[][] teach = getMergedDesiredOutputs();
+
+		List<Integer> toErase = new ArrayList<Integer>();
+		
+		// See which samples lie inside the given radius around the given coordinates. If so, do
+		// not add them to the new arrays.
+		// NOTE: For simplicity, the radius is NOT evaluated as a circle, but as a square. This is
+		//       sufficient for this application for now.
+		for(int n=0; n<in.length; n++) {
+			if (in[n][0] >= x - radius && in[n][0] <= x + radius &&
+					in[n][1] >= y - radius && in[n][1] <= y + radius) {
+				// Erase point (here, we just mark the index to be erased)
+				toErase.add(n);
+			}
+		}
+		
+		// Now, compose the new arrays and use them
+		double[][] nin = new double[in.length - toErase.size()][2];
+		double[][] nteach = new double[in.length - toErase.size()][1];
+		
+		int nn = 0;
+		for(int n=0; n<in.length; n++) {
+			if (!toErase.contains(n)) {
+				nin[nn] = in[n];
+				nteach[nn] = teach[n];
+				nn++;
+			}
+		}
+		
+		if (nin.length > 0) {
+			setLesson(new TrainingSampleLesson(nin, nteach));
+		} else {
+			setLesson(null);
+		}
+	}
+
+	@Override
+	public boolean hasData() {
+		return (trainingLesson != null || testLesson != null);
+	}
+
+	@Override
+	public void initialize() {
+		setLesson(null);
+	}
+
+	private void setLesson(TrainingSampleLesson lesson) {
+		trainingLesson = null;
+		testLesson = null;
+
+		if (lesson == null) {
+			// Reset all data to null: We are finished here
+			return;
+		}
+		
+		if(lesson.countSamples() < 10) {
+			// Less than ten samples: Create training data only
+			trainingLesson = lesson;
+			return;
+		}
+		
+		// More than 10 samples: Split lesson into training and test data
+		TrainingSampleLesson[] split = lesson.splitLesson(0.5); 
+		
+		trainingLesson = split[0];
+		testLesson = split[1];
+		
+		//this.lesson.optimizeDesiredOutputsForClassificationProblem(net);
+	}
+
+	@Override
+	public DataContainer getTrainingLesson() {
+		return getContainerFromTrainingLesson(trainingLesson);
+	}
+
+	@Override
+	public DataContainer getTestLesson() {
+		return getContainerFromTrainingLesson(testLesson);
+	}
+
+	@Override
+	public int getNumOfSamples(boolean training) {
+		if (training) {
+			if (trainingLesson == null) return 0;
+			return trainingLesson.countSamples();
+		} else {
+			if (testLesson == null) return 0;
+			return testLesson.countSamples();
+		}
+	}
+
+	private DataContainer getContainerFromTrainingLesson(TrainingSampleLesson lesson) {
+		if (lesson == null) return null;
+		return new DataContainer(lesson.getInputs(), lesson.getDesiredOutputs());
+	}
+
+	@Override
+	public DataContainer getCompleteDataContainer() {
+		if (!hasData()) return null;
+		return new DataContainer(getMergedInputs(), getMergedDesiredOutputs());
+	}
+
+	@Override
+	public void setFromCompleteDataContainer(DataContainer c) {
+		if (c == null) {
+			setLesson(null);
+			return;
+		}
+		setLesson(new TrainingSampleLesson(c.getInputs(), c.getDesiredOutputs()));
+	}
+
+	public TrainingSampleLesson getSNIPETrainingLesson() {
+		return trainingLesson;
+	}
+
+	public TrainingSampleLesson getSNIPETestLesson() {
+		return testLesson;
+	}
+
+	@Override
+	public void resplitData() {
+		if (!hasData()) return;
+		setLesson(new TrainingSampleLesson(getMergedInputs(), getMergedDesiredOutputs()));
+	}
+
+}
---
+++ src/de/tunetown/nnpg/model/neuroph/NeurophNetworkWrapper.java
@@ -0,0 +1,308 @@
+package de.tunetown.nnpg.model.neuroph;
+
+import org.neuroph.core.NeuralNetwork;
+
+import de.tunetown.nnpg.model.DataWrapper;
+import de.tunetown.nnpg.model.ModelProperties;
+import de.tunetown.nnpg.model.NetworkWrapper;
+
+/**
+ * Wrapper for SNIPE network.
+ * 
+ * @author xwebert
+ *
+ */
+public class NeurophNetworkWrapper extends NetworkWrapper {
+
+	private double eta = ModelProperties.NETWORK_DEFAULT_ETA;
+	private int batchSize = ModelProperties.NETWORK_DEFAULT_BATCHSIZE;
+	private double initialRange = ModelProperties.NETWORK_INITIAL_RANGE;
+
+	private NeuralNetwork net;
+/*
+	private int behavior = 0;
+	
+	private String[] behaviorDescriptions = { 
+			"TanH", 
+			"TanH (ACM)",
+			"TanH (Jafama)",
+			"Tanh (Ang.)", 
+			"TanH (AngLeCun)", 
+			"Tanh (LeCun)", 
+            "Fermi", 
+            "Identity", 
+            "LeakyInt.Lin.", 
+            "LeakyInt.Exp." };
+	
+	private NeuronBehavior[] behaviors = { 
+			new TangensHyperbolicus(),
+			new TangensHyperbolicusACM(),
+			new TangensHyperbolicusJafama(),
+			new TangensHyperbolicusAnguita(),
+			new TangensHyperbolicusAnguitaLeCun(),
+			new TangensHyperbolicusLeCun(),
+			new Fermi(),
+			new Identity(),
+			new LeakyIntegratorExponential(-1),
+			new LeakyIntegratorLinear(-1)};
+*/
+	public NeurophNetworkWrapper() {
+		createNetwork(ModelProperties.NETWORK_DEFAULT_TOPOLOGY);
+	}
+		
+	public NeurophNetworkWrapper(int[] topology) {
+		createNetwork(topology);
+	}
+	
+	public NeurophNetworkWrapper(int[] topology, double initialRange, int behavior) {
+		//this.behavior = behavior;
+		//this.initialRange = initialRange;
+		//createNetwork(topology, initialRange, behavior);
+	}
+
+	private void createNetwork() {
+		//createNetwork(ModelProperties.NETWORK_DEFAULT_TOPOLOGY, initialRange, behavior);
+	}
+	
+	@Override
+	public void createNetwork(int[] topology) {
+		//createNetwork(topology, initialRange, behavior);
+	}
+		
+	private void createNetwork(int[] topology, double initialRange, int behavior) {
+		/*
+		NeuralNetworkDescriptor desc = new NeuralNetworkDescriptor(topology);
+		desc.setSettingsTopologyFeedForward();
+		desc.setSynapseInitialRange(initialRange);
+		
+		desc.setNeuronBehaviorInputNeurons(new Identity());
+		desc.setNeuronBehaviorHiddenNeurons(behaviors[behavior]);
+		desc.setNeuronBehaviorOutputNeurons(behaviors[behavior]);
+
+		net = new NeuralNetwork(desc);
+		*/
+	}
+
+	@Override
+	public int countNeurons() {
+		return 0;
+	}
+
+	@Override
+	public int countLayers() {
+		return 0;
+	}
+
+	@Override
+	public int countNeuronsInLayer(int layer) {
+		return 0;
+	}
+
+	@Override
+	public boolean isSynapseExistent(int fromNeuron, int toNeuron) {
+		return false;
+	}
+
+	@Override
+	public double getWeight(int fromNeuron, int toNeuron) {
+		//if (!net.isSynapseExistent(fromNeuron + 1, toNeuron + 1)) return Double.NaN;
+		return 0; //net.getWeight(fromNeuron + 1,  toNeuron + 1);
+	}
+
+	@Override
+	public int getLayerOfNeuron(int num) {
+		return 0;
+	}
+
+	@Override
+	public int getFirstNeuronInLayer(int layer) {
+		return 0;
+	}
+
+	@Override
+	public double[] propagate(double[] in) {
+		return new double[1]; //net.propagate(in);
+	}
+
+	@Override
+	public void train(DataWrapper data) {
+		if (data.getTrainingLesson() == null || data.getTrainingLesson().size() == 0) return;
+
+		//TrainingSampleLesson lesson = ((NeurophDataWrapper)data).getSNIPETrainingLesson();
+		//net.trainBackpropagationOfError(lesson, batchSize, eta);
+	}
+
+	@Override
+	public double getTrainingError(DataWrapper data) {
+		//TrainingSampleLesson lesson = ((NeurophDataWrapper)data).getSNIPETrainingLesson();
+		//if (lesson == null || lesson.countSamples() == 0) return 0;
+		return 0; //ErrorMeasurement.getErrorSquaredPercentagePrechelt(net, lesson) / 100; //.getErrorRootMeanSquareSum(net, lesson);
+	}
+
+	@Override
+	public double getTestError(DataWrapper data) {
+		//TrainingSampleLesson lesson = ((NeurophDataWrapper)data).getSNIPETestLesson();
+		//if (lesson == null || lesson.countSamples() == 0) return 0;
+		return 0; //ErrorMeasurement.getErrorSquaredPercentagePrechelt(net, lesson) / 100; //.getErrorRootMeanSquareSum(net, lesson);
+	}
+
+	@Override
+	public double getBiasWeight(int num) {
+		//if (!net.isSynapseExistent(0, num + 1)) return Double.NaN;
+		return 0; //net.getWeight(0, num + 1);
+	}
+
+	@Override
+	public double getEta() {
+		return eta;
+	}
+
+	@Override
+	public void setEta(double eta) {
+		this.eta = eta;
+	}
+
+	@Override
+	public int getBatchSize() {
+		return batchSize;
+	}
+
+	@Override
+	public void setBatchSize(int size) {
+		batchSize = size;
+	}
+
+	@Override
+	public int[] getTopology() {
+		return new int[1]; //net.getDescriptor().getNeuronsPerLayer();
+	}
+
+	@Override
+	public int getMaxNeuronsInLayers() {
+		int max = 0;
+		for(int i=0; i<countLayers(); i++) {
+			if (countNeuronsInLayer(i) > max) max = countNeuronsInLayer(i);
+		}
+		return max;
+	}
+
+	@Override
+	public NetworkWrapper clone() {
+		//NeurophNetworkWrapper ret = new NeurophNetworkWrapper(net.getDescriptor().getNeuronsPerLayer(), initialRange, behavior);
+		//ret.net = net.clone();
+		//ret.setParametersFrom(this);
+		return null; //ret;
+	}
+
+	@Override
+	public void setParametersFrom(NetworkWrapper network) {
+		NeurophNetworkWrapper n = (NeurophNetworkWrapper)network;
+		
+		setEta(n.getEta());
+		setBatchSize(n.getBatchSize());
+		setBehavior(n.getBehavior());
+		setInitialRange(n.getInitialRange());
+	}
+
+	/**
+	 * NOTE: For SNIPE, the reset flag is ignored in setTopology()!
+	 */
+	@Override
+	public void addLayer(int position, int neurons, boolean reset) {
+		/*
+		if (position >= net.countLayers()) return;
+		
+		int[] t = net.getDescriptor().getNeuronsPerLayer();
+		int[] nt = new int[t.length + 1];
+		
+		int nn = 0;
+		for(int i=0; i<position; i++) {
+			nt[nn] = t[i];
+			nn++;
+		}
+		nt[nn] = neurons;
+		nn++;
+		for(int i=position; i<t.length; i++) {
+			nt[nn] = t[i];
+			nn++;
+		}
+		createNetwork(nt);
+		*/
+	}
+
+	/**
+	 * NOTE: For SNIPE, the reset flag is ignored in setTopology()!
+	 */
+	@Override
+	public void removeLayer(int layer, boolean reset) {
+		/*
+		if (layer >= net.countLayers()) return;
+		
+		int[] t = net.getDescriptor().getNeuronsPerLayer();
+		int[] nt = new int[t.length - 1];
+		
+		int nn = 0;
+		for(int i=0; i<layer; i++) {
+			nt[nn] = t[i];
+			nn++;
+		}
+		for(int i=layer+1; i<t.length; i++) {
+			nt[nn] = t[i];
+			nn++;
+		}
+		createNetwork(nt);
+		*/
+	}
+
+	@Override
+	public void addNeuron(int layer, boolean reset) {
+		/*
+		int[] t = net.getDescriptor().getNeuronsPerLayer();
+		if (layer >= t.length || layer < 0) return;
+		t[layer]++;
+		createNetwork(t);
+		*/
+	}
+
+	@Override
+	public void removeNeuron(int layer, boolean reset) {
+		/*
+		int[] t = net.getDescriptor().getNeuronsPerLayer();
+		if (layer >= t.length || layer < 0) return;
+		if (t[layer] < 2) return;
+		t[layer]--;
+		createNetwork(t);
+		*/
+	}
+
+	@Override
+	public void setBehavior(int i) {
+		/*
+		if (i < 0 || i >= behaviors.length) return;
+		if (i == behavior) return;
+		behavior = i;
+		createNetwork();
+		*/
+	}
+
+	@Override
+	public String[] getBehaviorDescriptions() {
+		return null; //behaviorDescriptions;
+	}
+
+	@Override
+	public int getBehavior() {
+		return 0; //behavior;
+	}
+
+	@Override
+	public void setInitialRange(double range) {
+		initialRange = range;
+	}
+
+	@Override
+	public double getInitialRange() {
+		return initialRange;
+	}
+}
+
